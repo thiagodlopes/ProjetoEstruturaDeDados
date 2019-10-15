49bac11b195471fed6b6ec04568d31c8b961c40d
thiagodlopes <49046047+thiagodlopes@users.noreply.github.com>
2019-10-15 16:39:50 -0300

    Agenda 1.0
    As 6 funções principais estão funcionando, faltando apenas implementar o filtro de exibição (baseado em ordem alfabética para Nomes e cronológica para datas de nascimento).
    As funções de excluir contato e limpar a lista foram inseridas mais recententemente.
    O problema com maiúsculas e minúsculas nas buscas também foi resolvido.

---
+++ AgendaDeContatos/ListaEncadeada/Contato.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda {
+    public class Contato {
+
+        public string Nome;
+        public string DataDeNascimento;
+        public string Cpf;
+        public string Endereco;
+        public string Telefone;
+        public string EMail;
+
+        public Contato(string nome, string dataDeNascimento, string cpf, string endereco, string telefone, string eMail) {
+
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+            Cpf = cpf;
+            Endereco = endereco;
+            Telefone = telefone;
+            EMail = eMail;
+
+        }
+    }
+        /*class Agenda {
+
+            public void InserirContato() {
+
+                var contato = new Contato("", "", "", "", "", "");
+
+                var agenda = new LinkedList<Contato>();
+                agenda.AddLast(contato);
+
+                Console.WriteLine(agenda.Count);
+                
+                foreach (var contatos in agenda) {
+
+                Console.WriteLine($"{contatos.Nome}{contatos.DataDeNascimento}{contatos.Cpf}{contatos.Endereco}{contatos.Telefone}{contatos.EMail}");
+
+                }
+                
+
+                }
+            }
+
+        }*/
+
+    }
+
